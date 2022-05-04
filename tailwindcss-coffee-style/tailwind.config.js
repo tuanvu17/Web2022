2a7f9b81dfa304b398a26bebf6e6825deb4ab6d8
tuanvu17 <tuanvu17mta@gmail.com>
2022-05-04 20:44:29 +0700

    add subheadline
    add subheadline

--- tailwindcss-coffee-style/tailwind.config.js
+++ tailwindcss-coffee-style/tailwind.config.js
@@ -6,7 +6,14 @@ module.exports = {
         Karla: ['Karla', 'sans-serif']
       },
       colors:{
-        'life-coffee':'#C89F94'
+        
+        'coffee':{
+          50:'#E8D6D0',
+          200:'#C89F94',
+          400:'#A25F4B',
+          600:'#744838',
+
+        }
       },
       keyframes: {
         slideDown: {
