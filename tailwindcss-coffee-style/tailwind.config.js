509564291948b947d47cdab784337d56e5a8a3c9
tuanvu17 <tuanvu17mta@gmail.com>
2022-05-08 09:19:50 +0700

    add image
    add image

--- tailwindcss-coffee-style/tailwind.config.js
+++ tailwindcss-coffee-style/tailwind.config.js
@@ -19,10 +19,15 @@ module.exports = {
         slideDown: {
           '0%' : { transform: 'translateY(-100%)' },
           '100%': { transform: 'translateY(0%)' },
+        },
+        fadeIn:{
+          from:{opacity: 0,},
+          to:{opacity:1,}
         }
       },
       animation: {
         slideDown: 'slideDown .6s ease-in-out',
+        fadeIn: 'fadeIn .4s ease-in-out',
       },
       backgroundImage: {
         'slider-bg':'url("./img/favicon/bg-coffee.jpg")'
